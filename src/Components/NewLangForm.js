a08785d35e112e4f0ea68f9cb551caf44facf15d
Dylan Hays <twentygx@outlook.com>
2022-11-19 13:20:46 -0600

    Build NewLang object

--- src/Components/NewLangForm.js
+++ src/Components/NewLangForm.js
@@ -3,32 +3,38 @@ import react from 'react';
 function NewLangForm({ addLang }) {
     function handleSubmit(e) {
         e.preventDefault();
-        const output = Array.from(() => {const { name, 
-            image, 
-            initialVersion, 
-            founded, 
-            latestRelease, 
-            latestReleaseDate, 
-            creator, 
-            company, 
-            description } = e.target.elements });
-        console.log(output);
-        addLang();
-        e.target.reset();
+        const output = e.target.elements;
+        const newLang = {};
+        let languageType = '';
+        for (let ele of output) {
+            if (ele.name == 'languageType') {
+                languageType = ele.value;
+            }
+            else if (ele.name != 'submit') {
+                newLang[ele.name] = ele.value;
+            }
+        }
+        console.log(languageType);
+        //addLang();
+        //e.target.reset();
     }
 
     return (
-        <form>
-            <input type="text" placeholder="Language Name" />
-            <input type="text" placeholder="Link to Lanuage Logo" />
-            <input type="text" placeholder="Initial Version" />
-            <input type="text" placeholder="Initial Release" />
-            <input type="text" placeholder="Latest Version" />
-            <input type="text" placeholder="Latest Release" />
-            <input type="text" placeholder="Creator" />
-            <input type="text" placeholder="Founding Company" />
-            <input type="text" placeholder="Description" />
-            <button type="submit">Add</button>
+        <form onSubmit={handleSubmit}>
+            <select name="languageType">
+                <option value="OOP">Object-Oriented</option>
+                <option value="Functional">Functional</option>
+            </select>
+            <input name='name' type="text" placeholder="Language Name" />
+            <input name='image' type="text" placeholder="Link to Lanuage Logo" />
+            <input name='initialVersion' type="text" placeholder="Initial Version" />
+            <input name='founded' type="text" placeholder="Initial Release" />
+            <input name='latestRelease' type="text" placeholder="Latest Version" />
+            <input name='latestReleaseDate' type="text" placeholder="Latest Release" />
+            <input name='creator' type="text" placeholder="Creator" />
+            <input name='company' type="text" placeholder="Founding Company" />
+            <input name='description' type="text" placeholder="Description" />
+            <button name='submit' type="submit">Add</button>
         </form>
     );
 }
