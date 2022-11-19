f337f9447607d69132f75bbe637009f6d8b2fae0
Dylan Hays <twentygx@outlook.com>
2022-11-18 21:02:46 -0600

    Removed table bc bad, replaced with grid

--- src/Components/LangList.js
+++ src/Components/LangList.js
@@ -6,24 +6,17 @@ import "../Style/LangList.css";
 function LangList({ Langs, LangType }) {
     let langRows = [];
     for (let i = 0; i < Langs.length; i++) {
-        if (i + 2 < Langs.length) {
-            langRows.push(Langs.slice(i, i + 3));
+        if (i + 3 < Langs.length) {
+            langRows.push(Langs.slice(i, i + 4));
         } else {
             langRows.push(Langs.slice(i));
         }
-        i += 2;
+        i += 3;
     }
 
     return (
         <div className="LangList">
-        <table className="LangTable">
-            <tbody>
-                {langRows.map(row =>
-                (<tr>
-                    {row.map(lang => <td><LangCard key={lang.name} props={lang}></LangCard></td>)}
-                </tr>))}
-            </tbody>
-        </table>
+            {Langs.map(x => (<div className="langCard"><LangCard key={x.id} props={x} /></div>))}
         </div>);
 }
 
